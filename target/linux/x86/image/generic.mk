c3cd3c37a401d833b71d4633cc77847785963eba
fenkail-ui <64298897+fenkail-ui@users.noreply.github.com>
2021-08-22 08:27:06 +0800

    Update generic.mk

--- target/linux/x86/image/generic.mk
+++ target/linux/x86/image/generic.mk
@@ -1,6 +1,6 @@
 define Device/generic
   DEVICE_TITLE := Generic x86
-  DEVICE_PACKAGES += kmod-8139too kmod-natsemi \
+  DEVICE_PACKAGES += kmod-8139too \
 	kmod-r8168 \
 	kmod-fs-vfat
   GRUB2_VARIANT := generic
